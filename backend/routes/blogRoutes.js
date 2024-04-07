571b57bf18a35a957fd3e210aabb856c3b7ea52f
akbarirazia <akbariharis14@gmail.com>
2024-04-07 11:04:32 +0430

    CRUD functions created

--- backend/routes/blogRoutes.js
+++ backend/routes/blogRoutes.js
@@ -5,6 +5,46 @@ const router = express.Router()
 
 router.get("/", (req, res, next) => {
   res.json(data)
+  res.status(200)
+})
+router.get("/:id", (req, res, next) => {
+  const { id } = req.params
+  const blog = data.posts.find((blog) => parseInt(blog.id) == parseInt(id))
+  res.json(blog)
+})
+
+router.post("/", (req, res, next) => {
+  const blog = req.body
+  data.posts.push(blog)
+  res.json(blog)
+})
+
+router.put("/:id", (req, res, next) => {
+  const updatedBlog = req.body
+  const { id } = req.params
+  const index = data.posts.findIndex(
+    (blog) => parseInt(blog.id) == parseInt(id)
+  )
+  if (index !== -1) {
+    data.posts[index] = { ...data.posts[index], ...updatedBlog }
+    res.json(data.posts[index])
+  } else {
+    res.status(404).json({ message: "Blog post not found" })
+  }
+})
+
+router.delete("/:id", (req, res, next) => {
+  const { id } = req.params
+  const index = data.posts.findIndex(
+    (blog) => parseInt(blog.id) === parseInt(id)
+  )
+
+  data.posts.splice(index, 1)
+  if (index !== -1) {
+    data.posts.splice(index, 1)
+  } else {
+    res.status(404).send("No such blog was found")
+  }
 })
 
 module.exports = router
